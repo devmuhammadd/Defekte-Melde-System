aab6f59d1eef68eb9209da9a6f617470f35ceb61
Muhammad Abuzar <abuzar9658@gmail.com>
2023-11-17 11:17:50 +0500

    Add /me endpoint

--- dms-backend/app/apis/v1/route_login.py
+++ dms-backend/app/apis/v1/route_login.py
@@ -1,7 +1,18 @@
+from app.core.config import settings
+from app.core.hashing import Hasher
+from app.core.security import create_access_token
+from app.db.repository.login import get_user
+from app.db.session import get_db
+from fastapi import APIRouter
+from fastapi import Depends
+from fastapi import HTTPException
+from fastapi import status
+from fastapi.security import OAuth2PasswordBearer
 from fastapi.security import OAuth2PasswordRequestForm
-from fastapi import Depends, APIRouter
+from jose import jwt
+from jose import JWTError
+from schemas.token import Token
 from sqlalchemy.orm import Session
-from fastapi import status, HTTPException
 
 from app.db.session import get_db
 from app.core.hashing import Hasher
@@ -32,3 +43,27 @@ def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db:
         data={"sub": user.email}
     )
     return {"token": {"access_token": access_token, "token_type": "bearer"}, "user": user}
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+    )
+
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+        )
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = get_user(email=username, db=db)
+    if user is None:
+        raise credentials_exception
+    return user
--- dms-backend/app/apis/v1/route_user.py
+++ dms-backend/app/apis/v1/route_user.py
@@ -1,14 +1,16 @@
-from typing import Union
-
-from app.core.security import create_access_token
-from app.db.repository.user import create_new_user
-from app.db.session import get_db
 from fastapi import APIRouter
 from fastapi import Depends
 from fastapi import status
-from app.schemas.user import UserCreate
-from app.schemas.user_token import UserToken
 from sqlalchemy.orm import Session
+
+from typing import Union
+from app.core.security import create_access_token
+from app.db.repository.user import create_new_user
+from app.db.session import get_db
+
+from app.schemas.user import UserCreate, ShowUser
+from app.schemas.user_token import UserToken
+from app.apis.v1.route_login import get_current_user
 
 router = APIRouter()
 
@@ -20,3 +22,10 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
         data={"sub": user.email}
     )
     return {"token": {"access_token": access_token, "token_type": "bearer"}, "user": user}
+
+
+@router.post("/me", response_model=ShowUser)
+def current_user(token: UserToken, 
+                 db: Session = Depends(get_db),
+                 current_user: ShowUser = Depends(get_current_user)):
+    return current_user
