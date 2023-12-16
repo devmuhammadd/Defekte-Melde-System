6ea1e1f7b6e59f902085415c754fb6f9ef22f86b
Hammad Saeed <hammad.saeed776@gmail.com>
2023-12-17 00:47:48 +0500

    Update side navbar to display option according to user role

--- dms-frontend/src/@core/layouts/components/vertical/navigation/VerticalNavItems.js
+++ dms-frontend/src/@core/layouts/components/vertical/navigation/VerticalNavItems.js
@@ -2,6 +2,7 @@
 import VerticalNavLink from './VerticalNavLink'
 import VerticalNavGroup from './VerticalNavGroup'
 import VerticalNavSectionTitle from './VerticalNavSectionTitle'
+import { useAuth } from 'src/hooks/useAuth'
 
 const resolveNavItemComponent = item => {
   if (item.sectionTitle) return VerticalNavSectionTitle
@@ -13,8 +14,10 @@ const resolveNavItemComponent = item => {
 const VerticalNavItems = props => {
   // ** Props
   const { verticalNavItems } = props
+  const { user } = useAuth();
 
-  const RenderMenuItems = verticalNavItems?.map((item, index) => {
+  const filteredNavItems = verticalNavItems?.filter(item => item.authority === 'everyone' || item.authority === user?.role);
+  const RenderMenuItems = filteredNavItems?.map((item, index) => {
     const TagName = resolveNavItemComponent(item)
 
     return <TagName {...props} key={index} item={item} />
