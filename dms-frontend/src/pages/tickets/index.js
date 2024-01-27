6ae069734205761b403de26f89fefe500ae0b164
Hammad Saeed <hammad.saeed776@gmail.com>
2024-01-27 07:24:08 +0500

    Add new page to show ticket

--- dms-frontend/src/pages/tickets/index.js
+++ dms-frontend/src/pages/tickets/index.js
@@ -24,7 +24,7 @@ const Home = ({ hasQueryParams }) => {
       } else if (editTicket && ticketId) {
         return <EditTicket ticketId={ticketId} />;
       } else if (ticketId) {
-        return <ShowTicket />
+        return <ShowTicket ticketId={ticketId} />
       }
     } else {
       return <Dashboard />;
--- dms-frontend/src/views/pages/tickets/ShowTicket.jsx
+++ dms-frontend/src/views/pages/tickets/ShowTicket.jsx
@@ -1,8 +1,76 @@
-import React from 'react'
+import { Card, CardContent, CardMedia, Grid, Paper, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import { useTicket } from 'src/hooks';
+import { useAuth } from 'src/hooks/useAuth';
 
-function ShowTicket() {
+function ShowTicket({ ticketId }) {
+  const { user } = useAuth();
+  const { tickets } = useTicket();
+  const [ticket, setTicket] = useState();
+
+  useEffect(() => {
+    setTicket(tickets.filter((ticket) => ticket?.id == ticketId)[0]);
+  }, [])
+
   return (
-    <div>ShowTicket</div>
+    <Grid container spacing={6}>
+      <Grid item xs={12}>
+        <Card>
+          <CardContent sx={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Grid xs={12} md={6}>
+              <Card>
+                {ticket?.image &&
+                  <CardMedia sx={{ height: '14.5625rem' }} image='/images/cards/glass-house.png' />
+                }
+                <CardContent>
+                  <TableContainer component={Paper}>
+                    <Table>
+                      <TableBody>
+                        <TableRow>
+                          <TableCell><strong>Title</strong></TableCell>
+                          <TableCell>{ticket?.title}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell><strong>Location</strong></TableCell>
+                          <TableCell>{ticket?.location}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell><strong>Urgency</strong></TableCell>
+                          <TableCell>{ticket?.urgency}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell><strong>Status</strong></TableCell>
+                          <TableCell>{ticket?.status}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell><strong>Reporter</strong></TableCell>
+                          <TableCell>{ticket?.userId === user?.id ? 'You' : ticket?.user}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell><strong>Mechanic</strong></TableCell>
+                          <TableCell>{ticket?.mechanic}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell><strong>Description</strong></TableCell>
+                          <TableCell>{ticket?.description}</TableCell>
+                        </TableRow>
+                      </TableBody>
+                    </Table>
+                  </TableContainer>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid xs={12} md={5}>
+              <Card>
+                <CardContent>
+                  <Typography variant='h5'>Comments</Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
   )
 }
 
